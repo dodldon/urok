55702d09ab8311f15b7091d5cf35a5b455f0851f
kde <kde@kde.dom>
2016-07-17 19:56:41 +0300

    16

---
+++ Argum_kom_stroki/src/argum_kom_stroki/Argum_kom_stroki.java
@@ -0,0 +1,12 @@
+
+package argum_kom_stroki;
+//Использование аргумента командной строки
+public class Argum_kom_stroki {
+
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++){
+     System.out.println("Равен[ "+args[i]+"]");
+    }
+    
+}
+}
