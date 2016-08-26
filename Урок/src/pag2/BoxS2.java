8c07257d3d22994327c2016ca71ac98b7f51d35f
mms <mms@localhost.localdomain>
2016-08-26 16:32:47 +0300

    26

--- Урок/src/pag2/BoxS2.java
+++ Урок/src/pag2/BoxS2.java
@@ -5,10 +5,10 @@ import pag.*;
 public class BoxS2 {
 
     public static void main(String[] args) {
-        BoxExt2   box1 = new BoxExt2.(10,20,34.2);
-        BoxExt2_1 box2 = new BoxExt2_1(2, 3,4,0.076);
-        System.out.println("Обьём box   = " + box1.volume());
-        System.out.println("  Вес box   = " + box2.weight);
+//        BoxExt2   box1 = new BoxExt2.(10,20,34.2);
+//        BoxExt2_1 box2 = new BoxExt2_1(2, 3,4,0.076);
+//        System.out.println("Обьём box   = " + box1.volume());
+//        System.out.println("  Вес box   = " + box2.weight);
     
 
     }
