823f3b56bbfb4fc2d7fd72dee1855e1a0778a260
mms <mms@localhost.localdomain>
2017-01-09 17:12:06 +0300

    Revert "Revert "3""
    This reverts commit 8b65cdfca8052a5e19f9af2557fdd9e522bfd70f.

--- Урок/src/javarus/Solution.java
+++ Урок/src/javarus/Solution.java
@@ -1,26 +1,38 @@
 package javarus;
-/* Зарплата через 5 лет
-Ввести с клавиатуры отдельно Имя, число1, число2. Вывести надпись:
-«Имя» получает «число1» через «число2» лет.
-Пример: Коля получает 3000 через 5 лет.
-*/
 
+/* Сортировка трех чисел
+Ввести с клавиатуры три числа, и вывести их в порядке убывания.
+ */
 import java.io.*;
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        InputStream inputStraem=System.in;
-        Reader inputStreamReader=new InputStreamReader (inputStraem);
-        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStraem));
-        int a;
-       String name=bufferedReader.readLine();
-       String nam=bufferedReader.readLine();
-       String na=bufferedReader.readLine();
-       int i=Integer.parseInt(nam);
-       int x=Integer.parseInt(na);
-       System.out.println(name + " получает "+i+" через "+x+" лет");
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+    
+        if (a > b && a > c) {
+            //m = n;
+            System.out.println(a + "" + b + "" + c);
+        }
+        if (a > c && a > b) {
+            System.out.println(a + "" + c + "" + b);
+        }
+        if (b > a && b > c) {
+            System.out.println(b + "" + a + "" + c);
+        }
+        if (b > c && b > a) {
+            System.out.println(b + "" + c + "" + a);
+        }
+        if (c > a && c < b) {
+            System.out.println(c + "" + a + "" + b);
+
+        }
+        if (c > b && c < a) {
+            System.out.println(c + "" + b + "" + a);
+        }
 
     }
 }
