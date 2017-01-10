da87246b759b39a644b8fd61a777d5262f226c66
mms <mms@localhost.localdomain>
2017-01-10 16:23:02 +0300

    21

---
+++ Урок/src/learn/Cat.java
@@ -0,0 +1,20 @@
+package learn;
+
+import java.io.*;
+
+public class Cat {
+
+    public static void main(String args[]) {
+
+        int i = 1, x = 2;
+
+        if (x > i && i > x) {
+            
+            System.out.println(i + " " + x);
+        } else {
+            System.out.println("else " + i + " " + x);
+        }
+
+    }
+
+}
