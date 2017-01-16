7cef8d40f90650a498bd39ff006deed8323fb3ff
mms <mms@localhost.localdomain>
2017-01-16 16:59:08 +0300

    Задача 8. Координатные четверти

--- Урок/src/javarus/Solution.java
+++ Урок/src/javarus/Solution.java
@@ -1,30 +1,28 @@
 package javarus;
 
-/* Три числа
- Ввести с клавиатуры три целых числа. 
- Одно из чисел отлично от двух других, равных между собой.
- Вывести на экран порядковый номер числа, отличного от остальных.
- Пример для чисел 4 6 6:
- 1
- Пример для чисел 6 6 3:
- 3
- */
+/* Координатные четверти
+Ввести с клавиатуры два целых числа, которые будут координатами точки, 
+не лежащей на координатных осях OX и OY.
+Вывести на экран номер координатной четверти, в которой находится данная точка.
+Подсказка:
+Принадлежность точки с координатами (a,b) к одной из четвертей определяется следующим образом:
+для первой четверти a>0 и b>0;
+для второй четверти a<0 и b>0;
+для третьей четверти a<0 и b<0;
+для четвертой четверти a>0 и b<0.
+Пример для чисел 4 6:
+1
+Пример для чисел -6 -6:
+3
+*/
+
 import java.io.*;
 
-public class Solution {
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int n1 = Integer.parseInt(br.readLine());
-        int n2 = Integer.parseInt(br.readLine());
-
-        if (n == n1 & n!= n2) {
-            System.out.println("3");
-        } else if (n1 == n2 & n1 != n) {
-            System.out.println("1");
-        } else if (n == n2 & n != n1) {
-            System.out.println("2");
-        }
     }
 }
