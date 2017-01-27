e7eefd0c4d77ba1c8cfde096656e99211bd77cec
mms <mms@localhost.localdomain>
2017-01-27 16:19:16 +0300

    Четные числа

--- Урок/src/learn/Learn.java
+++ Урок/src/learn/Learn.java
@@ -458,6 +458,19 @@ public class Solution {
         }
     }
 }
+/* Четные числа
+Используя цикл for вывести на экран чётные числа от 1 до 100 включительно.
+Через пробел либо с новой строки.
+    
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        for(int i=2;i<=100;i=i+2)
+            System.out.println(i);
+
+    }
+}
 
 */
 }
